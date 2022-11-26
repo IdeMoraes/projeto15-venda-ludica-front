db3ae3d9b95f832224d2280fdc667e2f8137a338
nandasino <fernandamsino@gmail.com>
2022-11-26 17:33:53 -0300

    Home com layout pronto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 //import {Link} from "react-router-dom";
 import { getProducts } from "../../services/api";
 //import axios from "axios"
+import Navbar from "./Navbar";
 
 
 function Home(){
@@ -29,6 +30,10 @@ function Home(){
         }
         setShowProducts(productsFilter);
     }
+    function filterAll(){
+        setShowProducts(products);
+        setMessage("");
+}
     function filterBooks(){
             const productsFilter = products.filter((element)=>element.department==="Livro");
             setShowProducts(productsFilter);
@@ -54,10 +59,8 @@ function Home(){
         setShowProducts(productsFilter);
         setMessage("");
     }
-    function reload(){
-        window.location.reload();
-    }
     return(
+        <>
         <Container>
         <form onSubmit={filterAray}>
         <input
@@ -65,19 +68,30 @@ function Home(){
         placeholder= "Pesquise pelo nome do produto"
         onChange= {(e)=>setSearch(e.target.value)}
         /> 
-        <button type="submit">pesquisar</button>
+        <button type="submit"><ion-icon name="search"></ion-icon></button>
         </form>
-        <button onClick={()=>filterBooks()}>Livros</button>
-        <button onClick={()=>filterAlbuns()}>Álbuns</button>
-        <button onClick={()=>filterJogos()}>Jogos</button>
-        <button onClick={()=>filterFilmes()}>Filmes</button>
-        <button onClick={()=>filterSeries()}>Séries de TV</button>
-        <div className={message.length!==0? "aparece": "some"}>
-            <h1>{message}</h1>
-            <h1 onClick={()=>reload()}>Voltar a comprar</h1>
-        </div>
-        <ul className={message.length===0? "aparece": "some"}>{showProducts.length!==0 ? showProducts.map((p)=><li>{p.name}</li>):products.map((p)=><li>{p.name}</li>)}</ul>
-        </Container>   
+        <Categorias>
+        <h1 onClick={()=>filterAll()}>Todos</h1>
+        <h1 onClick={()=>filterBooks()}>Livros</h1>
+        <h1 onClick={()=>filterAlbuns()}>Álbuns</h1>
+        <h1 onClick={()=>filterJogos()}>Jogos</h1>
+        <h1 onClick={()=>filterFilmes()}>Filmes</h1>
+        <h1 onClick={()=>filterSeries()}>Séries de TV</h1>
+        </Categorias>
+        <Message className={message.length!==0? "aparece": "some"}>
+            <h2>{message}</h2>
+            <h1 onClick={()=>filterAll()}>Voltar a comprar</h1>
+        </Message>
+        <ContainerProducts className={message.length===0? "aparece": "some"}>
+            {showProducts.length!==0 ? showProducts.map((p)=>
+            <Product><h1>{p.name}</h1><img src={p.image}/><h1>{p.price}</h1></Product>)
+            :
+            products.map((p)=>
+            <Product><h1>{p.name}</h1><img src={p.image}/><h1>{p.price}</h1></Product>)}
+        </ContainerProducts>
+        </Container>
+        <Navbar/>   
+        </>
     )
 }
 
@@ -86,9 +100,91 @@ export default Home;
 const Container = styled.div`
     display: flex;
     flex-direction:column;
-    height: 100vh;
+    align-items: center;
     width:100%;
+    min-height: 100vh;
+    background-color: #f4f4f4;
     .some{
         display:none;
+    }
+    form{
+        display: flex;
+        margin-bottom: 36px;
+        margin-top:40px;
+        justify-content:center;
+        align-items: center;
+    }
+    input {
+        display:flex;
+        align-items: center;
+        padding-left: 20px;
+        width: 300px;
+        height: 48px;
+        border-radius: 30px;
+        border:none;
+        box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+        background-color: white;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 15px;
+        box-sizing: border-box;
+      }
+      button{
+        border:none;
+        width:40px;
+        height: 40px;
+        background-color: #f4f4f4;
+        margin-left:5px;
+      }
+      ion-icon{
+        font-size: 30px;
+      }
+`
+const Categorias= styled.div`
+    width:100%;
+    display:flex;
+    justify-content:space-around;
+    margin-bottom: 36px;
+    h1{
+        cursor:pointer;
+        font-size:12px;
+    }
+`
+const Message = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items: center;
+    h1{
+        cursor:pointer;
+        font-size:20px;
+        margin-top:36px;
+    }
+`
+const ContainerProducts = styled.div`
+    display:flex;
+    flex-wrap: wrap;
+    justify-content:space-around;
+    h1{
+        cursor:pointer;
+        font-size:20px;
+    }
+`
+const Product =styled.div`
+    width:170px;
+    height:250px;
+    display:flex;
+    flex-direction:column;
+    align-items: center;
+    justify-content:space-around;
+    padding:8px;
+    border-radius: 5px;
+    border:none;
+    background-color:#ffffff;
+    margin-bottom:20px;
+    img{
+        width:100px;
+    }
+    h1{
+        font-size:10px;
     }
 `
