7cabdfab39caf0fe9a755e68c99aef457b722776
nandasino <fernandamsino@gmail.com>
2022-11-28 16:19:02 -0300

    front carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -9,6 +9,7 @@ import UserContext from "../../contexts/UserContext.js";
 function Cart(){
     const {userToken} = useContext(UserContext);
     const [cartProducts, setCartProducts] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         getCart(userToken)
           .then((response) => {
@@ -18,13 +19,58 @@ function Cart(){
           .catch((error) => console.log(error));
           console.log(userToken);
       }, []);
-    
-
+    function closeCart(){
+      navigate("/checkout");
+    }
     return(
         <>
-        <li>olaaa</li>
+        <Container>
+          <h1>Carrinho</h1>
+          {cartProducts.map((p,index)=>
+          <DivProduct>
+          <DivImagem>
+            {p.name}
+            <img src={p.image}/>
+          </DivImagem>
+          {p.quantity}
+          {/* <button onClick={()=>updateProduct(p.productId, p.quantity)}>+</button> */}
+          </DivProduct>
+          )}
+        <button onClick={()=>closeCart()}>Fechar Pedido</button>
+        </Container>
         <Navbar/>
         </>
     );
 }
 export default Cart;
+
+const Container = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f4f4f4;
+  min-height: 100vh;
+  h1{
+    margin-top:20px;
+    margin-bottom:20px;
+  }
+`
+const DivProduct= styled.div`
+  display:flex;
+  width:90%;
+  height: 250px;
+  justify-content:space-around;
+  background-color:#ffffff; 
+  align-items: center;
+  margin-bottom: 20px;
+  box-sizing:border-box;
+`
+const DivImagem = styled.div`
+  display:flex;
+  width:50%;
+  flex-direction: column;
+  img{
+    width:80%;
+  }
+`
