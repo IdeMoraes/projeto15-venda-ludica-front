8a441a7086c6d08e2747a5c1396ec21586cdb21e
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-11-28 17:52:34 -0300

    Troca url do back

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://projeto15-venda-ludica.onrender.com";
 
 /*export function createHeaders(){
     const config ={
