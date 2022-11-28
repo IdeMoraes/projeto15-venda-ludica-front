5a8c5af7db3333417112dfffd00a5ca1be1c7755
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-11-28 10:33:31 -0300

    Corrige a quantidade de cada produto pedido com parseInt()

--- src/services/api.js
+++ src/services/api.js
@@ -35,5 +35,5 @@ export function postCart(newProductToCart, token){
     const config = {
         headers: { Authorization: `Bearer ${token}` }
     };
-    const promise = axios.post(`${BASE_URL}/cart`, [newProductToCart], config)
+    const promise = axios.post(`${BASE_URL}/cart`, newProductToCart, config)
 };
